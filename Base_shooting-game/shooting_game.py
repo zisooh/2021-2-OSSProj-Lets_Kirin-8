e67eef111813693f3cac9f91ce6eaaa0707655b4
TaehoJung <teo325@naver.com>
2021-11-21 23:12:48 +0900

    Update

--- Base_shooting-game/shooting_game.py
+++ Base_shooting-game/shooting_game.py
@@ -307,12 +307,10 @@ def main():
 
     if selectMode == 'SingleMode':
         print('Single mode play')
-        while True:
-            Single.play()
+        Single.play()
     elif selectMode == 'TimeMode':
         print('Time mode play')
-        while True:
-            Time.play()
+        Time.play()
     elif selectMode == 'PvpMode':
         print('Pvp mode play')
         #Pvp.play()    
