33ecb8bc7670f9bcc3752fbcf40daa44021abf39
TaehoJung <teo325@naver.com>
2021-12-06 17:57:46 +0900

    fix field error

--- Codes/mode_pvp.py
+++ Codes/mode_pvp.py
@@ -636,8 +636,8 @@ class Pvp() :
                     field2Rect.midbottom = field1Rect.midtop
                 
                 field_size = (field1.get_width() * ratio, field1.get_height() * ratio)
-                screen.blit(pygame.transform.scale(field1, field_size), field1Rect)
-                screen.blit(pygame.transform.scale(field2, field_size), field2Rect)
+                screen.blit(pygame.transform.scale(field1, field_size), (0,field1Rect.y))
+                screen.blit(pygame.transform.scale(field2, field_size), (0,field2Rect.y))
                 pygame.draw.rect(screen, BLACK, [250,0,3,500])
 
             # Update and draw all sprites and text         
@@ -771,8 +771,8 @@ class Pvp() :
                 field2Rect.midbottom = field1Rect.midtop
             
             field_size = (field1.get_width() * ratio, field1.get_height() * ratio)
-            screen.blit(pygame.transform.scale(field1, field_size), field1Rect)
-            screen.blit(pygame.transform.scale(field2, field_size), field2Rect)
+            screen.blit(pygame.transform.scale(field1, field_size), (0,field1Rect.y))
+            screen.blit(pygame.transform.scale(field2, field_size), (0,field2Rect.y))
             pygame.draw.rect(screen, BLACK, [250,0,3,500])
 
         # Update and draw all sprites
